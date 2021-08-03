1a9c5f8b71fa1d3f5042291e9986218e4fdb5bff
ymxxx <738533403@qq.com>
2021-08-03 18:58:52 +0800

    xxx 803.py main方法

---
+++ pro0714/0723_ljb/0728/803.py
@@ -0,0 +1,19 @@
+
+class Main():
+    def __init__(self):
+        self.f1()
+
+    print(333)
+
+    def f1(self):
+        bb=4
+        print(bb)
+
+
+def main():
+    m=Main()
+    # m.f1()
+
+
+if __name__ == '__main__':
+    main()
